137742dea0aa3398c5acc69d1f3c90fbbc9f1e42
Conan Jung <45420008+ppijbb@users.noreply.github.com>
2024-07-31 14:03:03 +0900

    chore: Update setup.py

--- setup.py
+++ setup.py
@@ -13,9 +13,9 @@ except Exception as error:
 
 
 INSTALL_REQUIRES = [
-    "transformers >= 4.38.0",
+    "transformers >= 4.38.0, <5.0.0",
     "accelerate >= 0.23.0",
-    "optimum ~= 1.20.0",
+    "optimum >= 1.20.0, <2.0.0",
     "huggingface_hub >= 0.20.1",
     "numpy>=1.22.2, <=1.25.2",
     "protobuf<4",
